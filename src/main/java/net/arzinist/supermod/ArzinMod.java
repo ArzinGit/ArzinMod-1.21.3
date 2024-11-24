b11c22bc411015d73ab382e6f81c3d6460f86885
ArzinGit <iancaeldurden@gmail.com>
2024-11-23 19:55:22 -0500

    Initial commit

---
+++ src/main/java/net/arzinist/supermod/ArzinMod.java
@@ -0,0 +1,16 @@
+package net.arzinist.supermod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ArzinMod implements ModInitializer {
+	public static final String MOD_ID = "arzinmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/arzinist/supermod/ArzinModClient.java
@@ -0,0 +1,10 @@
+package net.arzinist.supermod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ArzinModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
